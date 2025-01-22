4f2f2620855f444a7b0dfa04e7ac89c7a3aca7f3
fhr*std <fhrustrator@gmail.com>
2025-01-22 22:05:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -38,3 +38,38 @@ async function fetchAnimations() {
 
 // Call the function to load the data and update the DOM
 fetchAnimations();
+
+// Use an async function to retrieve data
+async function fetchBackground() {
+  // Fetch data from the Supabase database
+  const { data: background, error } = await supabase
+    .from('background')
+    .select('target_id, gif_url, name');
+
+  // Check if there was an error fetching the data
+  if (error) {
+    console.error("Error fetching data:", error);
+    return;
+  }
+
+  // Find the <a-assets> container inside the <a-scene>
+  const assetsContainer = document.querySelector('#assets-container2');
+
+  // Loop through the retrieved data and create <img> tags inside the <a-assets> tag
+  background.forEach(item => {
+    const imgTag = document.createElement('img');
+    imgTag.setAttribute('id', item.name);   // Set 'id' to the animation's name
+    imgTag.setAttribute('src', item.gif_url);  // Set 'src' to the gif_url
+
+    // Append each <img> tag to the <a-assets> tag
+    assetsContainer.appendChild(imgTag);
+  // Now dynamically set the material of the box using the loaded asset
+      const bok = document.querySelector('#bok1');
+      bok.setAttribute('material', `src: #${item.name}`);
+  });
+  // Log the final HTML structure of <a-assets> to the console
+  console.log(assetsContainer.innerHTML);
+}
+
+// Call the function to load the data and update the DOM
+fetchBackground();
